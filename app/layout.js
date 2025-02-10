f40f1b60a35f1f97b31b6ce380befb7ecc91cb95
Enes Mete Kafali <emkafali@gmail.com>
2025-02-10 17:43:54 +0300

    Feat: Add Clerk Authentication

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,11 @@
 import Header from "@/components/header";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
-import { Inter } from "next/font/google"
+import { ThemeProvider } from "@/components/theme-provider";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { shadesOfPurple } from "@clerk/themes";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Zcrum",
@@ -12,20 +14,36 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          <Header />
-          <main className="min-h-screen">
-            {children}
-          </main>
-          <footer className="bg-gray-900 py-12">
-            <div className="container mx-auto px-4 text-center text-gray-200">
-              <p>Made with love by Enes Mete Kafali.</p>
-            </div>
-          </footer>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: shadesOfPurple,
+        variables: {
+          colorPrimary: "#3b82f6",
+          colorBackground: "#1a202c",
+          colorInputBackground: "#2D3748",
+          colorInputText: "#F3F4F6"
+        },
+        elements: {
+          formButtonPrimary: "text-white",
+          card: "bg-gray-800",
+          headerTitle: "text-blue-400",
+          headerSubtitle: "text-gray-400"
+        }
+      }}
+    >
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <ThemeProvider attribute="class" defaultTheme="dark">
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <footer className="bg-gray-900 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made with love by Enes Mete Kafali.</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
