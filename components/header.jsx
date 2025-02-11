2095fbb05d7a074597cdb31b01c77a2e35e2f524
Enes Mete Kafali <emkafali@gmail.com>
2025-02-11 09:53:22 +0300

    Feat: Add Header

--- components/header.jsx
+++ components/header.jsx
@@ -1,21 +1,42 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton
-} from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { PenBox } from "lucide-react";
+import UserMenu from "./user-menu";
 
 const Header = () => {
   return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
+    <header className="container mx-auto">
+      <nav className="py-6 px-4 flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src={"/logo2.png"}
+            alt="Zcrum Logo"
+            width={200}
+            height={56}
+            className="h-10 w-auto object-contain"
+          />
+        </Link>
+        <div className="flex items-ceter gap-4">
+          <Link href='/project/create'>
+            <Button variant="destructive" className="flex items-center gap-2">
+              <PenBox size={18} />
+              <span>Create Project</span>
+            </Button>
+          </Link>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/onboarding">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
 
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+          <SignedIn>
+            <UserMenu />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
   );
 };
 
